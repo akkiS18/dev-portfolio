6842a7d0c741facb6dd9bbed3f3d5baa0854503c
akkiS18 <llus77385@gmail.com>
2024-09-27 10:24:17 +0500

    some style changed and bugs fixed

--- js/script.js
+++ js/script.js
@@ -2,17 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
 	document.querySelector(".content").classList.add("loaded");
 
 	const work_open = document.getElementById("click_work");
-	const brand = document.getElementById("brand");
+	const main = document.getElementById("main");
+	const footer = document.getElementById("footer");
 	const back = document.getElementById("back");
 	const workElement = document.getElementById("work");
 	const envoy = document.getElementById("envoy");
 
 	work_open.addEventListener("click", () => {
 		workElement.classList.add("show_work");
+		main.classList.add("hide");
+		footer.classList.add("hide");
 	});
 
 	back.addEventListener("click", () => {
 		workElement.classList.remove("show_work");
+		main.classList.remove("hide");
+		footer.classList.remove("hide");
 	});
 
 	envoy.addEventListener("click", () => {
